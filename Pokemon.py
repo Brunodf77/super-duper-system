4c47d89cc7d42cf69230f3f72dd51f246565f338
bruno <beboy.milanac@hotmail.fr>
2024-12-10 12:43:42 +0100

    Exo1

---
+++ Pokemon.py
@@ -0,0 +1,15 @@
+pokedex = []
+def creationPokemon() :
+    pokemon = { 
+        "Nom" : input ("Selectionner un nom"),
+        "Type" : input ("Selectionner un type"),
+        "HP" : input ("Selectionner des HP"),
+        "Attaque" : input ("Selectionner une attaque"),
+        "Defense" : input ("Selectionner une Defense"),
+        "POWER" : input ("Selectionner un Power"),
+        "D" : input ("Selectionner un D"),
+        
+    }        
+
+def ajouteauPokedex(pokemon) : 
+    pokedex.append(pokemon) 
